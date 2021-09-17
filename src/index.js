82d6e3589c418b61094759dd150105d19cd3ddc7
Julio Mitac <mitacquispejuliocesar@gmail.com>
2021-09-17 03:15:46 -0500

    initial project

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import observable from "./observables/observable";
 import unsubscribe from "./observables/unsubcribe-add";
 import subject from "./observables/subject";
 import fromEvent from "./observables/fromEvent";
-//import exampleApi from "./example-api";
+import exampleApi from "./example-api";
 
 
-observable();
+//observable();
 //unsubscribe();
 //subject();
 //fromEvent();
-//exampleApi();
+exampleApi();
